56b63c03b746eb5835cc4ba60bda6109e7d35a70
dongbinkoo <rnehdqlsdbxbqm@gmail.com>
2025-01-22 19:48:55 +0900

    Upload iphone ui

--- src/components/MobileBtn.jsx
+++ src/components/MobileBtn.jsx
@@ -5,41 +5,43 @@ function MobileBtn({ handleToggle, isToggled }) {
     // 하나라도 true인지 확인
     return (
         <div className="navContainer">
-            <div className="navWeb">
-                <div
-                    className="navItem"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        handleToggle('weatherData');
-                    }}
-                >
-                    <Icon className={`faCloudMoon`} name="faCloudMoon" fontSize={'35px'} color={'white'} />
-                    <p>전국날씨</p>
+            <p>데이터 카테고리</p>
+            <div className="navWrap">
+                <div className="navWeb">
+                    <div
+                        className="navItem"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleToggle('weatherData');
+                        }}
+                    >
+                        <Icon className={`faCloudMoon`} name="faCloudMoon" fontSize={'38px'} color={'white'} />
+                        <p>전국날씨</p>
+                    </div>
                 </div>
-            </div>
-            <div className="navWeb">
-                <div
-                    className="navItem"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        handleToggle('airAlert');
-                    }}
-                >
-                    <Icon className={`faExclamation`} name="faExclamation" fontSize={'35px'} color={'white'} />
-                    <p>미세먼지</p>
+                <div className="navWeb">
+                    <div
+                        className="navItem"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleToggle('airAlert');
+                        }}
+                    >
+                        <Icon className={`faExclamation`} name="faExclamation" fontSize={'38px'} color={'white'} />
+                        <p>미세먼지</p>
+                    </div>
                 </div>
-            </div>
-
-            <div className="navWeb">
-                <div
-                    className="navItem"
-                    onClick={(e) => {
-                        e.stopPropagation();
-                        handleToggle('airMap');
-                    }}
-                >
-                    <Icon className={`faChartColumn`} name="faChartColumn" fontSize={'35px'} color={'white'} />
-                    <p>환경기준</p>
+                <div className="navWeb">
+                    <div
+                        className="navItem"
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            handleToggle('airMap');
+                        }}
+                    >
+                        <Icon className={`faChartColumn`} name="faChartColumn" fontSize={'38px'} color={'white'} />
+                        <p>환경기준</p>
+                    </div>
                 </div>
             </div>
         </div>
